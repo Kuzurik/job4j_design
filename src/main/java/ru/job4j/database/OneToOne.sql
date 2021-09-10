f489f0023ef92d1da7b4db8239896bde3a7ee782
Alex <vo>
2021-09-10 19:24:40 +0300

    0.1. Связи между таблицами [#373615]

--- src/main/java/ru/job4j/database/OneToOne.sql
+++ src/main/java/ru/job4j/database/OneToOne.sql
@@ -6,10 +6,10 @@ create table country(
 create table capital(
     id serial primary key,
     name varchar(255)
-)
+);
 
 create table country_capital(
-    id serial primary key
-    country_id int references country(id),
-    capital_id int references capital(id)
-)
+    id serial primary key,
+    country_id int references country(id) unique,
+    capital_id int references capital(id) unique
+);
